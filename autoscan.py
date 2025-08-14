e474c55dd1a7b91cfce1af401d34f1dcb6299bb1
zdenekchobot <zdenek.chobot@hotmail.com>
2025-08-15 00:07:02 +0200

    Formating changes to improve code readability and maintainability

--- autoscan.py
+++ autoscan.py
@@ -8,46 +8,46 @@ from cv2 import imreadmulti, imwritemulti, resize, INTER_AREA,IMWRITE_TIFF_COMPR
 
 from helpers import check_folders, get_unique_list_filename, move_file, set_logging, get_graph_access_headers, get_sharepoint_site_id, get_sharepoint_library_ids, get_list_columns, get_columns_mapping, normalize_qr_data, delete_file
 
-    # Nahrání konfiguračního souboru
+# Nahrání konfiguračního souboru
 os.chdir(os.getcwd())
 print(os.getcwd())
 with open("config.yaml", "r", encoding="utf-8") as f:
     cfg = yaml.safe_load(f)
 
-    #Kontrola a vytvoření složek
+#Kontrola a vytvoření složek
 check_folders(cfg)
 
-    #Iniciace logování
+#Iniciace logování
 log = set_logging(cfg)
 
-    # Hlavní program
+# Hlavní program
 log.info("Start")
 start_time = datetime.now()
 
-    # Získání přístupového tokenu M365 Graph, nastavení Bearer hlaviček
+# Získání přístupového tokenu M365 Graph, nastavení Bearer hlaviček
 graph_headers = get_graph_access_headers(cfg)
 
-    # Získání Sharepoint Site ID
+# Získání Sharepoint Site ID
 site_id = get_sharepoint_site_id(cfg, graph_headers)
 
-    # Získání Sharepoint library drive ID a list ID
+# Získání Sharepoint library drive ID a list ID
 drive_id, list_id = get_sharepoint_library_ids(cfg, site_id, graph_headers)
 
-    # Nastavení Sharepoint drive URL pro upload souborů
+# Nastavení Sharepoint drive URL pro upload souborů
 drive_url = f"{cfg['graph_api']['drives']}{drive_id}/items/root:/"
 
-    # Získání názvů sloupců library / listu ze Sharepoint
+# Získání názvů sloupců library / listu ze Sharepoint
 list_columns_names = get_list_columns(cfg, site_id, list_id, graph_headers)
 log.debug("Názvy sloupců listu Sharepointu: {0}".format(list_columns_names))
 
-    # Získání slovníku - mapy názvu sloupců name:displayName listu, ověření shody displayName z QR (konfigurační soubor) a listu Sharepointu  
+# Získání slovníku - mapy názvu sloupců name:displayName listu, ověření shody displayName z QR (konfigurační soubor) a listu Sharepointu  
 columns_names_map_dict = get_columns_mapping(cfg, list_columns_names)
 log.debug("Slovník mapování názvů sloupců a polí QR dat: {0}".format(columns_names_map_dict))
 
-    # Inicializace QReaderu - rozpoznávání QR kódů
+# Inicializace QReaderu - rozpoznávání QR kódů
 qr_reader = QReader()
 
-    # Vytvoření seznamu souborů skenů ke zpracování
+# Vytvoření seznamu souborů skenů ke zpracování
 files_for_processing = [
     os.path.join(cfg['app']['source_path'], file_name)
     for file_name in os.listdir(cfg['app']['source_path'])
@@ -55,7 +55,7 @@ files_for_processing = [
     and os.path.splitext(file_name)[1] in cfg['app']['allowed_extensions']
 ]
 
-    # Vyčištění složky "upload_path" - odstranění všech souborů
+# Vyčištění složky "upload_path" - odstranění všech souborů
 upload_files = os.listdir(cfg['app']['upload_path'])
 if len(upload_files) > 0:
     log.info("Vyprazdňuji složku {0}".format(cfg['app']['upload_path']))
@@ -65,13 +65,13 @@ if len(upload_files) > 0:
             os.remove(file_path)
             log.info("Smazán soubor {0}".format(file_path))  
 
-    # Zpracování skenů, příprava k uploadu do Sharepointu
+# Zpracování skenů, příprava k uploadu do Sharepointu
 for file in files_for_processing:
     log.info("Zpracovávám {0}".format(file))
-        # Načtení skenu ze soboru
+    # Načtení skenu ze soboru
     images = []
     ret, images = imreadmulti(mats=images, filename = file, flags=IMREAD_GRAYSCALE if cfg['app']['convert_to_grayscale'] else IMREAD_COLOR)
-        # Kontrola načtení skenu, v případě chyby přesunout soubor do "unprocessed_path" a pokračovat dalším
+    # Kontrola načtení skenu, v případě chyby přesunout soubor do "unprocessed_path" a pokračovat dalším
     if not ret:
         moved_file = move_file(cfg['app']['unprocessed_path'], file)
         log.error("Nepodařilo se načíst soubor {0}. Nezpracován, přesunut do {1}.".format(file, moved_file))
@@ -79,16 +79,16 @@ for file in files_for_processing:
     pages_count = len(images)
     log.info("Načteno stran: {0}".format(pages_count))
 
-        # Načtení QR kód z první strany skenu
+    # Načtení QR kód z první strany skenu
     qr_data = qr_reader.detect_and_decode(image=images[0])
     
-        # Pokud QR kód neexistuje, přesunout soubor do "unprocessed_path" a pokračovat dalším
+    # Pokud QR kód neexistuje, přesunout soubor do "unprocessed_path" a pokračovat dalším
     if qr_data == () or qr_data[0] is None:
         moved_file = move_file(cfg['app']['unprocessed_path'], file)
         log.error("V souboru {0} nerozpoznán QR kód. Nezpracován, přesunut do {1}".format(file, moved_file))
         continue
     
-        # Pokud první QR není validní (neobsahuje |), přesunout soubor do "unprocessed_path" a pokračovat dalším
+    # Pokud první QR není validní (neobsahuje |), přesunout soubor do "unprocessed_path" a pokračovat dalším
     if qr_data[0].find("|") == -1: 
         moved_file = move_file(cfg['app']['unprocessed_path'], file)
         log.error("V QR kódu souboru {0} nerozpoznána validní data. Nezpracován, přesunut do {1}.".format(file, moved_file))
@@ -98,12 +98,12 @@ for file in files_for_processing:
     qr_data_list = qr_data[0].split("|") # Převod QR dat na list, oddělovač "|"
     log.debug("Načteno {0} polí z QR kódu: {1}".format(len(qr_data_list),qr_data_list))
 
-        # Normalizace QR dat
+    # Normalizace QR dat
     qr_data_formated, dif = normalize_qr_data(cfg, qr_data_list)
     log.debug('Počet polí QR dat po normalizaci: {0}'.format(len(qr_data_formated)))
     log.debug('Normalizovaná QR data: {0}'.format(qr_data_formated))
 
-        # Změna velikosti skenů, je-li v konfiguračním souboru resize_scale různo od 1
+    # Změna velikosti skenů, je-li v konfiguračním souboru resize_scale různo od 1
     if cfg['app']['resize_scale'] != 1:
         log.debug("Zmenšuji velikost obrazů na {0} %".format(cfg['app']['resize_scale'] * 100))
         small_images = []
@@ -112,11 +112,11 @@ for file in files_for_processing:
             img_small = resize(img, None, fx=scale, fy=scale, interpolation=INTER_AREA)
             small_images.append(img_small)
 
-        # Unikátní název souboru pro upload do Sharepointu - pole 3 QR kódu "Číslo výrobní zakázky"
+    # Unikátní název souboru pro upload do Sharepointu - pole 3 QR kódu "Číslo výrobní zakázky"
     upload_file_name = get_unique_list_filename(f"{qr_data_list[2]}.tif", drive_url, graph_headers)
     log.debug("Unikátní název souboru pro upload: {0}".format(upload_file_name))
 
-        # Vytvoření souboru pro upload a uložení do "upload_path" 
+    # Vytvoření souboru pro upload a uložení do "upload_path" 
     upload_file_name_path = os.path.join(cfg['app']['upload_path'], upload_file_name)
     res = imwritemulti(upload_file_name_path, small_images, params=(IMWRITE_TIFF_COMPRESSION, 5))
     if res:
@@ -125,12 +125,12 @@ for file in files_for_processing:
         log.warning("Nepodařilo se vytvořit soubor {0} pro upload, pokračuji dalším souborem.".format(upload_file_name_path))
         continue    
 
-        # Nahrání souboru do Sharepointu
+    # Nahrání souboru do Sharepointu
     with open(upload_file_name_path, "rb") as f:
         upload_url = f"{drive_url}{upload_file_name}:/content"
         response = requests.put(upload_url, headers=graph_headers, data=f)
 
-            # Kontrola úspěšného nahrání souboru do Sharepointu. Při chybě přesun souboru do "unprocessed_path" a pokračovat dalším
+        # Kontrola úspěšného nahrání souboru do Sharepointu. Při chybě přesun souboru do "unprocessed_path" a pokračovat dalším
         if response.status_code != 201:  
             moved_file = move_file(cfg['app']['unprocessed_path'], upload_file_name_path)
             log.error("{0} se nepodařilo nahrát do Sharepointu, přesunut do {1}.".format(upload_file_name_path, moved_file))
@@ -138,13 +138,13 @@ for file in files_for_processing:
         else:
             log.info("Soubor {0} nahrán do Sharepointu.".format(upload_file_name_path))
 
-            # Získání id nahraného souboru
+        # Získání id nahraného souboru
         etag = response.json()["eTag"].lower()
         file_uuid = re.findall("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}", etag)[0]
         file_url = f"{cfg['graph_api']['drives']}{drive_id}/items/{file_uuid}/listItem?select=id"
         response = requests.get(file_url, headers=graph_headers)
         
-            # Kontrola úspěšného získání id nahraného souboru, při neúspěchu nelze nahrát metadata a pokračuje se dalším souborem
+        # Kontrola úspěšného získání id nahraného souboru, při neúspěchu nelze nahrát metadata a pokračuje se dalším souborem
         if response.status_code != 200:
             log.error("Nepodařilo se získat Sharepoint id nahraného souboru {0}, nelze uložit metadata. Pokračuji dalším souborem)".format(upload_file_name))
             continue
@@ -153,14 +153,14 @@ for file in files_for_processing:
             file_id = response.json()["id"]
             metadata_url = f"{cfg['graph_api']['sites']}{site_id}/lists/{list_id}/items/{file_id}/fields" # URL metadat (fields) nahraného souboru
 
-            # Nahrání metadat (fields) k uloženému souboru z qr_data_formated
+        # Nahrání metadat (fields) k uloženému souboru z qr_data_formated
         results = []
         idx = 0
         for column_name in columns_names_map_dict.keys():
             column_value = {}
             column_value[column_name] = qr_data_formated[idx]
         
-            # Nahrání metadat do sloupce Sharepointu, nejsou-li prázdná
+        # Nahrání metadat do sloupce Sharepointu, nejsou-li prázdná
             if qr_data_formated[idx] != "":
                 log.debug("Nahrávám metadate do sloupce {0}".format(columns_names_map_dict[column_name]))
                 metadata_response = requests.patch(metadata_url, headers=graph_headers, json=column_value)
@@ -176,7 +176,7 @@ for file in files_for_processing:
         else:
          log.warning("Nahráno pouze {0} z {1} metadat {2} ".format(results_ok_number, len(columns_names_map_dict), upload_file_name))
 
-        # Smazání originálního i zmenšeného souboru po úspěšném nahrání
+    # Smazání originálního i zmenšeného souboru po úspěšném nahrání
     log.info("{0} {1}".format(file, "smazán" if delete_file(file) else "neexistuje"))
     log.info("{0} {1}".format(upload_file_name_path, "smazán" if delete_file(upload_file_name_path) else "neexistuje"))
         
--- helpers.py
+++ helpers.py
@@ -4,7 +4,7 @@ import os
 import logging
 import requests
 
-    # Získání unikátního názvu souboru - pokud již existuje, přidá se číselný index (x)
+# Získání unikátního názvu souboru - pokud již existuje, přidá se číselný index (x)
 def get_unique_filename(file_name):
     idx = 0
     
@@ -18,7 +18,7 @@ def get_unique_filename(file_name):
         idx += 1
     return file_name
 
-    # Smazání souboru
+# Smazání souboru
 def delete_file(file_name):
     if os.path.exists(file_name):
         os.remove(file_name)
@@ -26,13 +26,13 @@ def delete_file(file_name):
     else:
         return False
 
-    # Kontrola existence názvu souboru v Sharepointu 
+# Kontrola existence názvu souboru v Sharepointu 
 def check_list_filename(file_name, drive_url, headers):
     name_url = drive_url + file_name + "?$select=name"
     response = requests.get(name_url, headers=headers)
     return False if response.status_code == 404 else True
 
-    # Získání unikátního názvu souboru v Sharepointu
+# Získání unikátního názvu souboru v Sharepointu
 def get_unique_list_filename(file_name, drive_url, headers):
     idx = 0
     f_name, f_ext = os.path.splitext(file_name)
@@ -41,14 +41,14 @@ def get_unique_list_filename(file_name, drive_url, headers):
         idx += 1
     return file_name
     
-    # Kontrola a vytvoření složek
+# Kontrola a vytvoření složek
 def check_folders(cfg):
     log_file_path, log_file_name = os.path.split(cfg['app']['log_file'])
     for path in [cfg['app']['unprocessed_path'], cfg['app']['upload_path'], log_file_path]:
         if not os.path.exists(path):
             os.makedirs(path)
 
-    # Nastavení logování
+# Nastavení logování
 def set_logging(cfg):
     log_format = logging.Formatter("%(asctime)s [%(levelname)-5.5s] %(message)s")
     log = logging.getLogger()
@@ -61,7 +61,7 @@ def set_logging(cfg):
     log.addHandler(console_log)
     return log
 
-    # Získání přístupového tokenu M365 Graph
+# Získání přístupového tokenu M365 Graph
 def get_graph_access_headers(cfg):
     auth_url = f"https://login.microsoftonline.com/{cfg['auth']['tenant_id']}/oauth2/v2.0/token"
     auth_data = {
@@ -81,7 +81,7 @@ def get_graph_access_headers(cfg):
     }
     return headers  
 
-    # Získání Sharepoint Site ID
+# Získání Sharepoint Site ID
 def get_sharepoint_site_id(cfg, headers):
     site_url = f"{cfg['graph_api']['sites']}{cfg['m365']['hostname']}:/sites/{cfg['m365']['site_name']}?select=sharepointIds"
     response = requests.get(site_url, headers=headers)
@@ -90,7 +90,7 @@ def get_sharepoint_site_id(cfg, headers):
     else:
         raise Exception("Nepodařilo se získat Site ID")
 
-    # Získání Sharepoint library drive ID a list ID
+# Získání Sharepoint library drive ID a list ID
 def get_sharepoint_library_ids(cfg, site_id, headers):
     library_url = f"{cfg['graph_api']['sites']}{site_id}/drives?$expand=list&search=name:{cfg['m365']['library_name']}"
     response = requests.get(library_url, headers=headers)
@@ -101,7 +101,7 @@ def get_sharepoint_library_ids(cfg, site_id, headers):
     else:
         raise Exception("Nepodařilo se získat library drive nebo list ID")
     
-    # Získání názvù sloupců knihovny/listu
+# Získání názvù sloupců knihovny/listu
 def get_list_columns(cfg, site_id, list_id, headers):
     library_columns_url = f"{cfg['graph_api']['sites']}{site_id}/lists/{list_id}/columns?$select=displayName,name"
     response = requests.get(library_columns_url, headers=headers)
@@ -110,19 +110,19 @@ def get_list_columns(cfg, site_id, list_id, headers):
     else:
         raise Exception("Nepodařilo se získat seznam sloupců knihovny / listu")
 
-    # Získání mapy názvu sloupců name:displayName
+# Získání mapy názvu sloupců name:displayName
 def get_columns_mapping(cfg, list_columns_names):
-        # Kontrola konfiguračního souboru
+    # Kontrola konfiguračního souboru
     if not cfg['m365']['column_names']:
         raise Exception("V konfiguračním souboru chybí názvy sloupců pro mapování.")
     
-        # Vytvoření slovníku pro mapování názvů sloupců
+    # Vytvoření slovníku pro mapování názvů sloupců
     list_columns_names_dict = {item["displayName"]: item["name"] for item in list_columns_names}
     
-        # Vytvoření prázdného slovníku - mapy displayName:name sloupců knihovny
+    # Vytvoření prázdného slovníku - mapy displayName:name sloupců knihovny
     columns_map_dict = {}
     
-        # Ověření existence názvu sloupce Konfigurační soubor vs. Sharepoint list, naplnění mapy   
+    # Ověření existence názvu sloupce Konfigurační soubor vs. Sharepoint list, naplnění mapy   
     for column_name in cfg['m365']['column_names']:
         if column_name in list_columns_names_dict:
             columns_map_dict[list_columns_names_dict[column_name]] = column_name
@@ -130,31 +130,31 @@ def get_columns_mapping(cfg, list_columns_names):
             raise Exception("Nepodařilo se namapovat sloupec {0} na sloupec v Sharepointu.".format(column_name))
     return columns_map_dict
 
-    # Přesunutí souboru do "destination_path" s přejmenováním souboru na unikátní jméno, pokud již existuje
+# Přesunutí souboru do "destination_path" s přejmenováním souboru na unikátní jméno, pokud již existuje
 def move_file(destination_path, file_name):
     target_file_name = os.path.join(destination_path, os.path.basename(file_name))
     
-        # Kontrola, zda soubor již existuje v cílové složce. Pokud ano, změní target_file_name na unikátní jméno
+    # Kontrola, zda soubor již existuje v cílové složce. Pokud ano, změní target_file_name na unikátní jméno
     if os.path.exists(target_file_name):
         target_file_name = get_unique_filename(target_file_name)
     
     os.rename(file_name, target_file_name)
     return target_file_name
 
-    # Normalizace QR dat
+# Normalizace QR dat
 def normalize_qr_data(cfg, qr_data_list):   
-        # Vymazání nežádoucích QR dat
+    # Vymazání nežádoucích QR dat
     qr_data_list.remove("$")
     
-        # Normalizace délky listu doplněním polí s mezerami podle počtu sloupců v konfiguračním souboru listu Sharepointu
+    # Normalizace délky listu doplněním polí s mezerami podle počtu sloupců v konfiguračním souboru listu Sharepointu
     dif = len(cfg['m365']['column_names']) - len(qr_data_list)
     if dif > 0:
         qr_data_list.extend([" "] * dif)
 
-        # Náhrada prázdných hodnot mezerami
+    # Náhrada prázdných hodnot mezerami
     qr_data_list = [item if item != "" else " " for item in qr_data_list]
 
-    # Převod datumu "Uvažovaný termín balení" na ISO formát
+# Převod datumu "Uvažovaný termín balení" na ISO formát
     data_idx = 5
     if (len(re.findall(r"[0|1|2|3]?[0-9]\.[0|1]?[0-9]\.[1|2][0-9]{3}", qr_data_list[data_idx])) >= 0): # Je hodnota datum ve formátu dd.mm.yyyy?
         iso_date = datetime.strptime(qr_data_list[data_idx], "%d.%m.%Y").isoformat()
